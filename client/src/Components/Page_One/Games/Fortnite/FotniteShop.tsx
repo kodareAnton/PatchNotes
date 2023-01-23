50159172be27e013164643f63ea92aa7f79b2c0d
Anton <antontorkkeli@yahoo.com>
2023-01-23 05:17:13 +0100

    push change

--- client/src/Components/Page_One/Games/Fortnite/FotniteShop.tsx
+++ client/src/Components/Page_One/Games/Fortnite/FotniteShop.tsx
@@ -2,36 +2,41 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 export function FortniteShop() {
-  interface Shop {
-    daily: {
-      entries: {
-        banner: null;
-        bundle: null;
-        categories?: [];
-        devName: string;
-        displayAssetPath: null;
-        finalPrice: number;
-        giftable: boolean;
-        items: {
-          id: string;
-        };
-      };
-    };
-    date: string;
-    featured: {};
-    hash: string;
-    specialDaily?: string;
-    specialFeatured?: {};
-    vbuckIcon: string;
-    voteWinners?: string;
-    votes?: string;
-  }
+ 
+interface News {
+status: 200,
+data: {
+  hash: string,
+  date: String,
+  image: string,
 
+  motds: [ {
+    id: string,
+  title: string,
+  tabTitle: string,
+  body: string,
+  image: string,
+  tileImage: string,
+  sortingPriority: string,
+  hidden: boolean,
+  }
+  ]
+
+  messages: [{
+    title: string
+    body: string
+    image: string
+    adspace: string
+  }]
+}
+}
+
+// skriv ut uppdateringar vi https://dash.fortnite-api.com/
   const [Shop, setShop] = useState();
 
   useEffect(() => {
     axios
-      .get("https://fortnite-api.com/v2/Shop/br")
+      .get("https://fortnite-api.com/v2/news")
       .then((res) => {
         console.log(res.data.data);
         setShop(res.data.data);
