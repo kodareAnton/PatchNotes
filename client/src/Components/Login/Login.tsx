96cc83508e328c33b9447483565d47bbfb3d9547
Anton <94136841+kodareAnton@users.noreply.github.com>
2023-01-24 21:33:29 +0100

    login

--- client/src/Components/Login/Login.tsx
+++ client/src/Components/Login/Login.tsx
@@ -1,14 +1,33 @@
+import { stripBasename } from '@remix-run/router';
+import { useState } from 'react';
 import './Login.css';
 
 export function Login() {
+  const [Name, setName] = useState('');
+  const [Password, setPassword] = useState('');
+
+  const LoggaIn = (e: any) => {
+    e.preventDefault();
+    localStorage.setItem(Name, Password);
+  };
+
   return (
     <div id="Login">
-      <form action="">
-        <label htmlFor="">Namn:</label>
-        <input type="text" />
+      <form onSubmit={LoggaIn}>
+        <label>Namn:</label>
+        <input
+          type="text"
+          value={Name}
+          onChange={(e) => setName(e.target.value)}
+        />
 
-        <label htmlFor="">Lössenord:</label>
-        <input type="text" />
+        <label>Lössenord:</label>
+        <input
+          type="text"
+          value={Password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <input type="submit" />
       </form>
     </div>
   );
