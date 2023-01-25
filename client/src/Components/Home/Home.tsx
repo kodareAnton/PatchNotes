afa9ce7b7a292fea874fa459889a0f1da37ad3e4
Anton <94136841+kodareAnton@users.noreply.github.com>
2023-01-25 19:20:17 +0100

    styling improve

--- client/src/Components/Home/Home.tsx
+++ client/src/Components/Home/Home.tsx
@@ -3,13 +3,30 @@ import './Home.css';
 export function Home() {
   return (
     <>
-      <div id="HomeDiv">
-        <h2>Se uppdateringar i ditt favoritspel här!</h2>
+      <div className="placementDiv">
+        <div id="HomeDiv">
+          <h2>Updateringar!</h2>
+          <p>
+            lalalala lal lal al la lal llllalala lalalalalalal l all ll la lal{' '}
+          </p>
+          <button>Login</button>
+          <button>Updates</button>
+        </div>
       </div>
 
       <div className="HomeCards">
-        <h2>News1</h2>
-        <p>bababababababababab vbababa ba bababab aaba babab abab abab </p>
+        <div className="HomeCard">
+          <h2>News1</h2>
+          <p>bababababababababab vbababa ba bababab aaba babab abab abab </p>
+        </div>
+        <div className="HomeCard">
+          <h2>News2</h2>
+          <p>bababababababababab vbababa ba bababab aaba babab abab abab </p>
+        </div>
+        <div className="HomeCard">
+          <h2>News3</h2>
+          <p>bababababababababab vbababa ba bababab aaba babab abab abab </p>
+        </div>
       </div>
     </>
   );
