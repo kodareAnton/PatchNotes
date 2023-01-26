08addd60753090b6479c59b101a63b6ac79931bd
Anton <94136841+kodareAnton@users.noreply.github.com>
2023-01-26 12:17:05 +0100

    loggin start

--- client/src/Components/Login/Login.tsx
+++ client/src/Components/Login/Login.tsx
@@ -1,5 +1,6 @@
 import { redirect, stripBasename } from '@remix-run/router';
 import { useState } from 'react';
+import { User } from 'react-feather';
 import { useNavigate } from 'react-router-dom';
 import './Login.css';
 
@@ -8,18 +9,39 @@ export function Login() {
   const [Password, setPassword] = useState('');
 
   const LoggaIn = (e: any) => {
+    const User = {
+      id: 3,
+      name: Name,
+      password: Password,
+    };
+    const Users = [
+      {
+        id: 1,
+        name: 'Anton',
+        password: 'test',
+      },
+      {
+        id: 2,
+        name: 'Jonas',
+        password: '123',
+      },
+    ];
     e.preventDefault();
-    console.log('Username', Name);
-    localStorage.setItem('Username', Name);
 
-    var getLocalStorageId = localStorage.getItem('Username');
+    //hittar i local storage
+    localStorage.setItem('User', JSON.stringify(Users));
 
-    if ((getLocalStorageId === '')) {
-      console.log('Fail Login');
-      return;
+    // hämtat localStorage
+    var getLocalStorageId = JSON.parse(localStorage.getItem('User') || '[]');
+
+    console.log(getLocalStorageId);
+
+    // inloggning
+    if (User.name === Users[0].name && User.password === Users[0].password) {
+      console.log('cool');
+      localStorage.setItem('userId', User.id);
     } else {
-      console.log('Login susses');
-      window.location.replace('/Home');
+      console.log('not cool');
     }
   };
 
