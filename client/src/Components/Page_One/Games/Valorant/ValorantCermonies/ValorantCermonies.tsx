4034ce6e5d349d5c597958d16823f591818790bf
Anton <94136841+kodareAnton@users.noreply.github.com>
2023-02-06 10:43:42 +0100

    push

--- client/src/Components/Page_One/Games/Valorant/ValorantCermonies/ValorantCermonies.tsx
+++ client/src/Components/Page_One/Games/Valorant/ValorantCermonies/ValorantCermonies.tsx
@@ -1,4 +1,43 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './ValorantCermonies.css';
-export function ValorantCermonies(){
-    return<><h1>ValorantCermonies</h1></>
+export function ValorantCermonies() {
+  interface ICermonies {
+    status: string;
+    data: [
+      {
+        uuid: string;
+        displayName: string;
+        assetPath: string;
+      }
+    ];
+  }
+
+  const [APICermonies, setAPICermonies] = useState<ICermonies>();
+
+  useEffect(() => {
+    axios
+      .get('https://valorant-api.com/v1/ceremonies')
+      .then((res) => {
+        setAPICermonies(res.data);
+        console.log(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <>
+      <h1>ValorantCermonies</h1>
+
+      {APICermonies?.data.map((cermonie) => {
+        return (
+          <>
+            <h2>{cermonie.displayName}</h2>
+          </>
+        );
+      })}
+    </>
+  );
 }
