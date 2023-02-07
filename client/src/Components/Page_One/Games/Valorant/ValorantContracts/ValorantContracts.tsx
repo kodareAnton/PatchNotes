5c3abfb7cdcb5b1c850bbe7d8634b67f31e633ef
Anton <94136841+kodareAnton@users.noreply.github.com>
2023-02-07 14:10:41 +0100

    push

--- client/src/Components/Page_One/Games/Valorant/ValorantContracts/ValorantContracts.tsx
+++ client/src/Components/Page_One/Games/Valorant/ValorantContracts/ValorantContracts.tsx
@@ -1,3 +1,68 @@
-export function ValorantContracts(){
-    return<></>
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+export function ValorantContracts() {
+  interface IContracts {
+    status: string;
+    data: [
+      {
+        uuid: string;
+        displayName: string;
+        displayIcon: string;
+        shipIt: boolean;
+        freeRewardScheduleUuid: string;
+        content: {
+          relationType: string;
+          relationUuid: string;
+          chapters: [
+            {
+              isEpilogue: boolean;
+              levels: [
+                {
+                  reward: {
+                    type: string;
+                    uuid: string;
+                    amount: number;
+                    isHighlighted: boolean;
+                  };
+                  xp: number;
+                  vpCost: number;
+                  isPurchasableWithVP: boolean;
+                }
+              ];
+              freeRewards: [
+                {
+                  type: string;
+                  uuid: string;
+                  amount: string;
+                  isHighlighted: boolean;
+                }
+              ];
+            }
+          ];
+          premiumRewardScheduleUuid: string;
+          premiumVPCost: number;
+        };
+        assetPath: string;
+      }
+    ];
+  }
+
+  const [APIContracts, setAPIContracts] = useState<IContracts>();
+
+  useEffect(() => {
+    if (APIContracts) {
+      if (APIContracts.data.length > 0) return;
+    } else
+      axios
+        .get('https://valorant-api.com/v1/contracts')
+        .then((res) => {
+          setAPIContracts(res.data);
+          console.log(res.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+  }, []);
+  return <></>;
 }
--- client/src/Components/Page_One/Games/Valorant/ValorantCurrencies/ValorantCurrencies.tsx
+++ client/src/Components/Page_One/Games/Valorant/ValorantCurrencies/ValorantCurrencies.tsx
@@ -1,3 +1,7 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import './ValorantCurrencies.css';
+
 export function ValorantCurrencies() {
   interface ICurrencies {
     status: string;
@@ -13,21 +17,34 @@ export function ValorantCurrencies() {
     ];
   }
 
-  //   const [APITiers, setAPITiers] = useState<ITiers>();
+  const [APICurrencies, setAPICurrencies] = useState<ICurrencies>();
 
-  //   useEffect(() => {
-  //     if (APITiers) {
-  //         if (APITiers.data.length > 0) return;
-  //       } else
-  //     axios
-  //       .get('https://valorant-api.com/v1/contenttiers')
-  //       .then((res) => {
-  //         setAPITiers(res.data);
-  //         console.log(res.data);
-  //       })
-  //       .catch((error) => {
-  //         console.log(error);
-  //       });
-  //   }, []);
-  return <></>;
+  useEffect(() => {
+    if (APICurrencies) {
+      if (APICurrencies.data.length > 0) return;
+    } else
+      axios
+        .get('https://valorant-api.com/v1/currencies')
+        .then((res) => {
+          setAPICurrencies(res.data);
+          console.log(res.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+  }, []);
+
+  return (
+    <div className="currenciesBox">
+      {APICurrencies?.data.map((currencies) => {
+        return (
+          <div key={currencies.displayName}>
+            <h2>{currencies.displayName}</h2>
+            <img src={currencies.displayIcon} alt={currencies.displayName} />
+            <p>{currencies.displayNameSingular}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
