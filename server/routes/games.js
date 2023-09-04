f7ba21519aa74b7ea25fdb913001eb48c3ccf519
Anton <94136841+kodareAnton@users.noreply.github.com>
2023-09-04 21:52:08 +0200

    push

--- server/routes/games.js
+++ server/routes/games.js
@@ -3,6 +3,7 @@ var router = express.Router();
 const axios = require('axios');
 const cheerio = require('cheerio');
 
+// Diablo 4 end tag
 router.get('/d4', function (req, res, next){
 apiUrl = 'https://news.blizzard.com/en-us/diablo4/23964909/diablo-iv-patch-notes'
 
@@ -22,6 +23,7 @@ axios.get(apiUrl)
 
 })
 
+// CounterStrike end tag
 router.get('/cs', function (req, res, next){
     apiUrl = 'https://blog.counter-strike.net/index.php/category/updates/'
     
@@ -36,17 +38,18 @@ router.get('/cs', function (req, res, next){
       // Handle any errors that occurred during the request
       console.error('Error:', error);
     });
-    })
+})
 
+// OverWatch 2 end tag
 router.get('/overwatch', function (req, res, next){
                 apiUrl = 'https://overwatch.blizzard.com/en-us/news/patch-notes/'
                 
                 // Make a GET request
                 axios.get(apiUrl)
                 .then(response => {
-                    const htmlContent = response.data;
-                    
-                    res.send(htmlContent)
+                  const htmlContent = response.data;
+                  
+                  res.send(htmlContent)
                 })
                 .catch(error => {
                 // Handle any errors that occurred during the request
