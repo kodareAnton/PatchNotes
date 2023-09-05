8367fc38adda29e858b603aef4044682406af450
Anton <94136841+kodareAnton@users.noreply.github.com>
2023-09-05 19:24:07 +0200

    push

--- server/routes/games.js
+++ server/routes/games.js
@@ -2,75 +2,75 @@ var express = require('express');
 var router = express.Router();
 const axios = require('axios');
 const cheerio = require('cheerio');
+var HTMLParser = require('node-html-parser');
 
-router.get('/', function (req, res, next){
+router.get('/', function (req, res, next) {
   const Html = ` 
   <h1>Välkommen till min Patchnotes backend!</h1>
-  `
-  res.send(Html)
-})
-
-// Diablo 4 end tag
-router.get('/d4', function (req, res, next){
-const apiUrl = 'https://news.blizzard.com/en-us/diablo4/23964909/diablo-iv-patch-notes';
-
-// Make a GET request
-axios.get(apiUrl)
-.then(response => {
-    const htmlContent = response.data;
-    const $ = cheerio.load(htmlContent)
-    
-    const patchnotes = $('.ResponsiveBlogs').html();
-
-    const p = $('summary').html();
-    console.log(p);
-
-    res.send(patchnotes)
-})
-.catch(error => {
-  // Handle any errors that occurred during the request
-  console.error('Error:', error);
+  `;
+  res.send(Html);
 });
 
-})
+// Diablo 4 end tag
+router.get('/d4', function (req, res, next) {
+  const apiUrl =
+    'https://news.blizzard.com/en-us/diablo4/23964909/diablo-iv-patch-notes';
 
-// CounterStrike end tag
-router.get('/cs', function (req, res, next){
-    const apiUrl = 'https://blog.counter-strike.net/index.php/category/updates/'
-    
-    // Make a GET request
-    axios.get(apiUrl)
-    .then(response => {
+  // Make a GET request
+  axios
+    .get(apiUrl)
+    .then((response) => {
+      const htmlContent = response.data;
 
-        const htmlContent = response.data;
-        // changing 
-        const $ = cheerio.load(htmlContent)
-        
-        const patchnotes = $('#main_blog').html();
+      // const $ = cheerio.load(htmlContent)
+      // const patchnotes = $('.ResponsiveBlogs').html();
+      // const p = $('summary').html();
+      // console.log(p);
 
-        res.send(patchnotes)
+      res.json('D5');
     })
-    .catch(error => {
+    .catch((error) => {
       // Handle any errors that occurred during the request
       console.error('Error:', error);
     });
-})
+});
+
+// CounterStrike end tag
+router.get('/cs', function (req, res, next) {
+  const apiUrl = 'https://blog.counter-strike.net/index.php/category/updates/';
+
+  // Make a GET request
+  axios
+    .get(apiUrl)
+    .then((response) => {
+      var patchArray = ['anton', 'david', 'oliver'];
+
+      var test = HTMLParser.parse(response);
+
+      res.json(patchArray);
+    })
+    .catch((error) => {
+      // Handle any errors that occurred during the request
+      console.error('Error:', error);
+    });
+});
 
 // OverWatch 2 end tag
-router.get('/overwatch', function (req, res, next){
-                apiUrl = 'https://overwatch.blizzard.com/en-us/news/patch-notes/'
-                
-                // Make a GET request
-                axios.get(apiUrl)
-                .then(response => {
-                  const htmlContent = response.data;
-                  
-                  res.send(htmlContent)
-                })
-                .catch(error => {
-                // Handle any errors that occurred during the request
-                console.error('Error:', error);
-                });
-})
+router.get('/overwatch', function (req, res, next) {
+  apiUrl = 'https://overwatch.blizzard.com/en-us/news/patch-notes/';
+
+  // Make a GET request
+  axios
+    .get(apiUrl)
+    .then((response) => {
+      const htmlContent = response.data;
+
+      res.send(htmlContent);
+    })
+    .catch((error) => {
+      // Handle any errors that occurred during the request
+      console.error('Error:', error);
+    });
+});
 
 module.exports = router;
